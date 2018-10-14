07a27fd9ac80ce67c4a4eb9c9ce626ca672cd2bd
DESKTOP-0OBAMA3\Sadaq-l <srmegh84@gmail.com>
2018-10-14 19:52:32 +0600

    no message

--- translators/Old Bailey Online.js
+++ translators/Old Bailey Online.js
@@ -9,7 +9,7 @@
 	"priority": 100,
 	"inRepository": true,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2018-09-28 16:25:00"
+	"lastUpdated": "2018-10-08 13:10:00"
 }
 
 /*
@@ -32,7 +32,7 @@
 	along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
 	
 	***** END LICENSE BLOCK *****
-	
+*/ 	
 // URLs
 // trial: div=t18000115-12
 // session: name=16900430
